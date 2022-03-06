dcdf5f6c094158e090f4a2343027037ef4002ebd
Mchi_yuki <metalsngke@gmail.com>
2022-03-06 19:10:13 +0900

    Lesson3 Sample3

---
+++ Lesson3/Sample3/Sample3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2;
+
+	num1 = 3;
+
+	cout << "変数num1の値は" << num1 << "です。";
+
+	num2 = num1;
+
+	cout << "変数num1の値を変数num2に代入しました。\n";
+	cout << "変数num2の値は" << num2 << "です。";
+
+	return 0;
+}
