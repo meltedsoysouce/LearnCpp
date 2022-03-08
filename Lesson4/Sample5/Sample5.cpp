999927e09ab3c857b2f7fe7a4644912facd25926
Mchi_yuki <metalsngke@gmail.com>
2022-03-08 18:35:42 +0900

    Lesson3 Sample5

---
+++ Lesson4/Sample5/Sample5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+
+	b = a++;
+
+	cout << "代入後にインクリメントしたのでbの値は" << b << "です。\n";
+
+	a = 0;
+	b = 0;
+
+	b = ++a;
+
+	cout << "代入前にインクリメントしたのでbの値は" << b << "です。\n";
+
+	return 0;
+}
