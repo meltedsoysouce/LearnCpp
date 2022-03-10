245e8108f897c04caa6f532ae30dfb5268edbac3
Mchi_yuki <metalsngke@gmail.com>
2022-03-10 17:57:31 +0900

    Lesson6 Sample3

---
+++ Lesson6/Sample3/Sample3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+
+	cout << "いくつ*を出力しますか?\n";
+
+	cin >> num;
+
+	for (int i = 0; i < num; i++)
+	{
+		cout << '*';
+	}
+
+	cout << '\n';
+
+	return 0;
+}
