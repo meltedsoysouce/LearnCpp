af05b16d8b4f6d5b55faa25027fbd44eb1027b18
Mchi_yuki <metalsngke@gmail.com>
2022-03-10 19:39:28 +0900

    Lesson7 Sample5

---
+++ Lesson7/Sample4/Sample4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+void buy(int x)
+{
+	cout << x << "万円の車を買いました。\n";
+}
+
+int main()
+{
+	int num;
+
+	cout << "1台目はいくらの車を買いますか？\n";
+	cin >> num;
+
+	buy(num);
+
+	cout << "2台目はいくらの車を買いますか？\n";
+	cin >> num;
+
+	buy(num);
+
+	return 0;
+}
