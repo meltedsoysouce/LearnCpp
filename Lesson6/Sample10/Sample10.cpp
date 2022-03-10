2188dddb89335f203a4be5d7f3fc1f3bcab0352e
Mchi_yuki <metalsngke@gmail.com>
2022-03-10 18:13:22 +0900

    Lesson6 Sample10

---
+++ Lesson6/Sample10/Sample10.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int res;
+
+	cout << "2何番目でループを中止しますか？\n";
+
+	cin >> res;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << i << "番目の処理です。\n";
+		if (res == i)
+			break;
+	}
+
+	return 0;
+}
