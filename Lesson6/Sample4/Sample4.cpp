00a11365c921a6503b5e111e0c12291a966f455d
Mchi_yuki <metalsngke@gmail.com>
2022-03-10 18:00:09 +0900

    Lesson6 Sample4

---
+++ Lesson6/Sample4/Sample4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	int sum = 0;
+
+	cout << "いくつまでの合計を求めますか？\n";
+
+	cin >> num;
+
+	for (int i = 0; i < num; i++)
+		sum += i+1;
+
+	cout << "1から" << num << "までの合計は" << sum << "です。\n";
+
+	return 0;
+}
