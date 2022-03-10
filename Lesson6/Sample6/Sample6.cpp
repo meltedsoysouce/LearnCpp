06ce590f655b48330576317e876e00d1e9289606
Mchi_yuki <metalsngke@gmail.com>
2022-03-10 18:06:36 +0900

    Lesson6 Sample6

---
+++ Lesson6/Sample6/Sample6.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num = 1;
+
+	while (num)
+	{
+		cout << "整数を入力してください。(0で終了)\n";
+		cin >> num;
+		cout << num << "が入力されました。\n";
+	}
+
+	cout << "繰り越しが終わりました。\n";
+
+	return 0;
+}
