58cc73262dede1f0ce3a6dd1e4f19c7d3cb4542b
Mchi_yuki <metalsngke@gmail.com>
2022-03-10 18:19:17 +0900

    Lesson6 sample12

---
+++ Lesson6/Sample12/Sample12.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int res;
+
+	cout << "何番目の処理を飛ばしますか？\n";
+
+	cin >> res;
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (i == res)
+			continue;
+		cout << i << "番目の処理です。\n";
+	}
+
+	return 0;
+}
