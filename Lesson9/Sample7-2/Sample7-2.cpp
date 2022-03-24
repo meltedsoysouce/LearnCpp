e026119598f126c595e279d883a6ad17ca196d22
Mchi_yuki <metalsngke@gmail.com>
2022-03-24 11:51:59 +0900

    Lesson9 Sample7-2

---
+++ Lesson9/Sample7-2/Sample7-2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+double avg(int* pT);
+
+int main()
+{
+	int test[5];
+
+	cout << "5人のテストの点数を入力してください。\n";
+	for (int i = 0; i < 5; i++)
+		cin >> test[i];
+
+	double ans = avg(test);
+
+	cout << "5人の平均点は" << ans << "です。\n";
+}
+
+double avg(int* pT)
+{
+	double sum = 0;
+	for (int i = 0; i < 5; i++)
+		sum += *(pT + i);
+
+	return sum / 5;
+}
