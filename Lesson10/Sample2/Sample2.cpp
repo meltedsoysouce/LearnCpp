11de1aaf7aac4d9761dfdc7dcd7c7da9a957624a
Mchi_yuki <metalsngke@gmail.com>
2022-03-26 22:37:00 +0900

    Lesson10 Sample2

---
+++ Lesson10/Sample2/Sample2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+void func();
+
+int a = 0;
+
+int main()
+{
+	for (int i = 0; i < 5; i++)
+		func();
+
+	return 0;
+}
+
+void func()
+{
+	int b = 0;
+	static int c = 0;
+
+	cout << "変数aは" << a << "変数bは" << b << "変数cは" << c << "です。\n";
+	a++;
+	b++;
+	c++;
+}
