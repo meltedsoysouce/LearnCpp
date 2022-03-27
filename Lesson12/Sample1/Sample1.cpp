6c2f70905387301da3ec1de13622d11ec4462704
Mchi_yuki <metalsngke@gmail.com>
2022-03-27 14:57:55 +0900

    Lesson12 Sample1

---
+++ Lesson12/Sample1/Sample1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+class Car {
+public:
+	int num;
+	double gas;
+	void show();
+};
+
+// クラスメンバ関数の定義
+void Car::show()
+{
+	cout << "車のナンバーは" << num << "です。\n";
+	cout << "ガソリン量は" << gas << "です。\n";
+}
+
+int main()
+{
+	Car car1;
+
+	car1.num = 1234;
+	car1.gas = 20.5;
+
+	car1.show();
+
+	return 0;
+}
